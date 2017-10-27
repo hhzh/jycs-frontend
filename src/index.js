2c91dbbd4334ea99a49800e608b6aae803c3179e
hhzh <1138253815@qq.com>
2017-10-27 18:40:09 +0800

    init.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<Index/>, document.getElementById('mainContainer'));
